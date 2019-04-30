8eef34ee184d05189d93f61d9d2acfea1fa641c8
tong.xu <tong.xu@quvideo.com>
2019-04-30 17:31:47 +0800

    update readme.md

--- lib_user/src/main/java/com/bee/lib/user/UserActivity.java
+++ lib_user/src/main/java/com/bee/lib/user/UserActivity.java
@@ -15,7 +15,7 @@ public class UserActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_main);
+        setContentView(R.layout.layout_user);
 
     }
 }
